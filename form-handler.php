faa9121f2db0e1d67677ae6a100cb4668ee33cb1
Nana Ike <kisaacnana@gmail.com>
2024-01-19 03:42:32 +0000

    new

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_from = 'fgciwg@gmail.com';
+$email_from = 'https://kisaacnana.github.io/CHURCH/';
 
 $email_subject = 'New Form Submission';
 
