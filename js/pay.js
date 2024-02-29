af8a5a776484ddb583ed59d3b5826516b99a2108
litoguy <bethelfiagbetor15@gmail.com>
2024-02-29 11:54:24 +0000

    Payment

--- js/pay.js
+++ js/pay.js
@@ -18,7 +18,7 @@ function payWithPaystack(e) {
 
     currency: 'GHS',
 
-    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    ref: document.getElementById("reference").value + Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
 
     // label: "Optional string that replaces customer email"
 
